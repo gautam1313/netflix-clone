bef7f0d80204c91009399f6edbb86befabd84ae6
Sebby80 <sebkm80@gmail.com>
2021-10-26 14:57:10 +0530

    Adding dummy video snippet on hover

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -4,17 +4,23 @@ import {
 } from "@material-ui/icons";
 import "./list.scss";
 import ListItem from "../listItem/ListItem";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 const List = () => {
+  const [slideNumber, setSlideNumber] = useState(0);
+  const [isMoved, setIsMoved] = useState(false);
+
   const listRef = useRef();
   const handleClick = (direction) => {
+    setIsMoved(true);
     let distance = listRef.current.getBoundingClientRect().x - 50;
 
-    if (direction === "left") {
+    if (direction === "left" && slideNumber > 0) {
+      setSlideNumber(slideNumber - 1);
       listRef.current.style.transform = `translateX(${distance + 230}px)`;
     }
-    if (direction === "right") {
+    if (direction === "right" && slideNumber < 4) {
+      setSlideNumber(slideNumber + 1);
       listRef.current.style.transform = `translateX(${distance - 230}px)`;
     }
   };
@@ -26,18 +32,19 @@ const List = () => {
         <ArrowBackIosOutlined
           className="sliderArrow left"
           onClick={() => handleClick("left")}
+          style={{ display: (!isMoved || slideNumber === 0) && "none" }}
         />
         <div className="container" ref={listRef}>
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
+          <ListItem index={0} />
+          <ListItem index={1} />
+          <ListItem index={2} />
+          <ListItem index={3} />
+          <ListItem index={4} />
+          <ListItem index={5} />
+          <ListItem index={6} />
+          <ListItem index={7} />
+          <ListItem index={8} />
+          <ListItem index={9} />
         </div>
         <ArrowForwardIosOutlined
           className="sliderArrow right"
