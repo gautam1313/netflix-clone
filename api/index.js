12a3b5e79ea9b5190c774f74919a7c543821f189
Sebby80 <sebkm80@gmail.com>
2021-11-06 17:53:05 +0530

    API models

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection successful"))
+  .catch((error) => console.log(error));
+
 app.listen(3002, () => {
   console.log("Netflix clone api backend connection success!");
 });
