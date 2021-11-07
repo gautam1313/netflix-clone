88756ed0208a764f8c8dc3ced909f4dce35e6fb7
Sebby80 <sebkm80@gmail.com>
2021-11-07 16:38:01 +0530

    Login user api request

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,11 +1,15 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const CryptoJS = require("crypto-js");
 
 //Register
 router.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
-    password: req.body.password,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.SECRET_KEY
+    ).toString(),
     email: req.body.email,
   });
 
@@ -14,6 +18,24 @@ router.post("/register", async (req, res) => {
     res.status(201).json(user);
   } catch (error) {
     res.status(500).json(err);
+  }
+});
+
+//Login
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(401).json("Wrong password or username");
+
+    var bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+    var originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+    originalPassword !== req.body.password &&
+      res.status(401).json("Wrong password or username");
+
+    const { password, ...userDetails } = user._doc;
+    res.status(200).json(userDetails);
+  } catch (error) {
+    res.status(500).json(error);
   }
 });
 
