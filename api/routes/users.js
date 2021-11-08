3e7cdbb85bdf593d2f95343bee05d9f4c8757340
Sebby80 <sebkm80@gmail.com>
2021-11-08 17:23:02 +0530

    User Rest API

--- api/routes/users.js
+++ api/routes/users.js
@@ -55,4 +55,60 @@ router.get("/find/:id", async (req, res) => {
   }
 });
 
+//GET all users (for admin)
+router.get("/", verify, async (req, res) => {
+  const query = req.query.new;
+  if (req.user.isAdmin) {
+    try {
+      const users = query
+        ? await User.find().sort({ _id: -1 }).limit(10)
+        : await User.find();
+      res.status(200).json(users);
+    } catch (error) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed to view");
+  }
+});
+
+//GET stats
+router.get("/stats", async (req, res) => {
+  const today = new Date();
+  const lastYear = today.setFullYear(today.setFullYear() - 1);
+  const monthsArray = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  try {
+    const data = await User.aggregate([
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: 1 },
+        },
+      },
+    ]);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 module.exports = router;
