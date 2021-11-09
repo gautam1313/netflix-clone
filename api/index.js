e8d0ea1bf86f0b538c2b7e3e0e5abbecec1e4d15
Sebby80 <sebkm80@gmail.com>
2021-11-09 18:54:50 +0530

    Movie lists api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const app = express();
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const movieRoute = require("./routes/movies");
+const listRoute = require("./routes/lists");
 
 require("dotenv").config();
 
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/movies", movieRoute);
+app.use("/api/lists", listRoute);
 
 app.listen(3002, () => {
   console.log("Netflix clone api backend connection success!");
