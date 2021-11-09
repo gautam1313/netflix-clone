db179c452de6a3b4570f1890b7be789f3e873c81
Sebby80 <sebkm80@gmail.com>
2021-11-09 19:53:42 +0530

    Adding router to navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./app.scss";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
+// return <Login />;
+// return <Register />;
+// return <Watch />;
 function App() {
-  return <Login />;
-  // return <Register />;
-  // return <Watch />;
-  // return <Home />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/movies" element={<Home type="movies" />} />
+        <Route path="/series" element={<Home type="series" />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
