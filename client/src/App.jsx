a33f8a8ce71cae75e1eef73c5b9cee1e0f8ce94f
Sebby80 <sebkm80@gmail.com>
2021-11-10 12:42:59 +0530

    Redirecting users to Home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,41 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import "./app.scss";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
-// return <Login />;
-// return <Register />;
+
 // return <Watch />;
 function App() {
+  const user = true;
   return (
     <Router>
       <Routes>
-        <Route path="/movies" element={<Home type="movies" />} />
-        <Route path="/series" element={<Home type="series" />} />
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/register"
+          element={!user ? <Register /> : <Navigate replace to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!user ? <Login /> : <Navigate replace to="/" />}
+        />
+        <Route
+          exact
+          path="/"
+          element={user ? <Home /> : <Navigate replace to="/register" />}
+        />
+        {user && (
+          <>
+            <Route path="/movies" element={<Home type="movies" />} />
+            <Route path="/series" element={<Home type="series" />} />
+            <Route path="/watch" element={<Watch />} />
+          </>
+        )}
       </Routes>
     </Router>
   );
