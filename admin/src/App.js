490b0064f493f3cae99055470f2e6926b818af18
Sebby80 <sebkm80@gmail.com>
2021-11-14 17:19:23 +0530

    Adding login page

--- admin/src/App.js
+++ admin/src/App.js
@@ -9,38 +9,46 @@ import NewUser from "./pages/newUser/NewUser";
 import ProductList from "./pages/productList/ProductList";
 import Product from "./pages/product/Product";
 import NewProduct from "./pages/newProduct/NewProduct";
+import Login from "./pages/login/Login";
 
 function App() {
+  const isLoggedOut = true;
   return (
-    <Router>
-      <Topbar />
-      <div className="container">
-        <Sidebar />
-        <Switch>
-          <Route path="/" exact>
-            <Home />
-          </Route>
-          <Route path="/users" exact>
-            <UserList />
-          </Route>
-          <Route path="/users/:userId" exact>
-            <User />
-          </Route>
-          <Route path="/newUser" exact>
-            <NewUser />
-          </Route>
-          <Route path="/movies" exact>
-            <ProductList />
-          </Route>
-          <Route path="/products/:productId" exact>
-            <Product />
-          </Route>
-          <Route path="/newProduct" exact>
-            <NewProduct />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <>
+      {isLoggedOut ? (
+        <Login />
+      ) : (
+        <Router>
+          <Topbar />
+          <div className="container">
+            <Sidebar />
+            <Switch>
+              <Route path="/" exact>
+                <Home />
+              </Route>
+              <Route path="/users" exact>
+                <UserList />
+              </Route>
+              <Route path="/users/:userId" exact>
+                <User />
+              </Route>
+              <Route path="/newUser" exact>
+                <NewUser />
+              </Route>
+              <Route path="/movies" exact>
+                <ProductList />
+              </Route>
+              <Route path="/products/:productId" exact>
+                <Product />
+              </Route>
+              <Route path="/newProduct" exact>
+                <NewProduct />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      )}
+    </>
   );
 }
 
