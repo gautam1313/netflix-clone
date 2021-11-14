110235b67809fb63f8ef60678cabbaf082a022e8
Sebby80 <sebkm80@gmail.com>
2021-11-14 16:15:45 +0530

    Updating monthly users activities on chart

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -1,18 +1,64 @@
 import Chart from "../../components/chart/Chart";
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
 import "./home.css";
-import { userData } from "../../dummyData";
 import WidgetSm from "../../components/widgetSm/WidgetSm";
 import WidgetLg from "../../components/widgetLg/WidgetLg";
+import axios from "axios";
+import { useEffect, useMemo, useState } from "react";
 
 const Home = () => {
+  const [userStats, setUserStats] = useState([]);
+  const MONTHS = useMemo(
+    () => [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    []
+  );
+
+  useEffect(() => {
+    const getStats = async () => {
+      let isApiSubscribed = true;
+      if (isApiSubscribed) {
+        const res = await axios.get(`/users/stats`, {
+          headers: {
+            token: `Bearer ${process.env.REACT_APP_TOKENID}`,
+          },
+        });
+        const sortedStats = res.data.sort(function (a, b) {
+          return a._id - b._id;
+        });
+        sortedStats.map((item) =>
+          setUserStats((prev) => [
+            ...prev,
+            { name: MONTHS[item._id - 1], "New User": item.total },
+          ])
+        );
+      }
+      return () => {
+        isApiSubscribed = false;
+      };
+    };
+    getStats();
+  }, [MONTHS]);
+
   return (
     <div className="home">
       <FeaturedInfo />
       <Chart
-        title="Sales Analytics"
-        data={userData}
-        dataKey="Active User"
+        title="Monthly Users Analytics"
+        data={userStats}
+        dataKey="New User"
         grid
       />
       <div className="homeWidgets">
