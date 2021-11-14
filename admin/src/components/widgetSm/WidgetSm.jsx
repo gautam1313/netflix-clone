b6f6d2c0859949e19d013c09544fba3e22aeeefe
Sebby80 <sebkm80@gmail.com>
2021-11-14 16:14:04 +0530

    Fetching and displaying all users from API

--- admin/src/components/widgetSm/WidgetSm.jsx
+++ admin/src/components/widgetSm/WidgetSm.jsx
@@ -1,86 +1,52 @@
 import { Visibility } from "@material-ui/icons";
 import "./widgetSm.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const WidgetSm = () => {
+  const [newUsers, setNewUsers] = useState([]);
+
+  useEffect(() => {
+    const getNewUsers = async () => {
+      let isApiSubscribed = true;
+      if (isApiSubscribed) {
+        const res = await axios.get("/users?new=true", {
+          headers: {
+            token: `Bearer ${process.env.REACT_APP_TOKENID}`,
+          },
+        });
+        setNewUsers(res.data);
+      }
+      return () => {
+        isApiSubscribed = false;
+      };
+    };
+    getNewUsers();
+  }, []);
+
   return (
     <div className="widgetSm">
       <span className="widgetSmTitle">New Members</span>
       <ul className="widgetSmList">
-        <li className="widgetSmListItem">
-          <img
-            src="https://pbs.twimg.com/media/ED9i1pXXsAAxpOh?format=jpg&name=medium"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername">Kimi Raikkonen</span>
-            <span className="widgetSmUserTitle">F1 Driver</span>
-          </div>
-          <button className="widgetSmButton">
-            <Visibility className="widgetSmIcon" />
-            Display
-          </button>
-        </li>
-        <li className="widgetSmListItem">
-          <img
-            src="https://pbs.twimg.com/media/ED9i1pXXsAAxpOh?format=jpg&name=medium"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername">Kimi Raikkonen</span>
-            <span className="widgetSmUserTitle">F1 Driver</span>
-          </div>
-          <button className="widgetSmButton">
-            <Visibility className="widgetSmIcon" />
-            Display
-          </button>
-        </li>
-        <li className="widgetSmListItem">
-          <img
-            src="https://pbs.twimg.com/media/ED9i1pXXsAAxpOh?format=jpg&name=medium"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername">Kimi Raikkonen</span>
-            <span className="widgetSmUserTitle">F1 Driver</span>
-          </div>
-          <button className="widgetSmButton">
-            <Visibility className="widgetSmIcon" />
-            Display
-          </button>
-        </li>
-        <li className="widgetSmListItem">
-          <img
-            src="https://pbs.twimg.com/media/ED9i1pXXsAAxpOh?format=jpg&name=medium"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername">Kimi Raikkonen</span>
-            <span className="widgetSmUserTitle">F1 Driver</span>
-          </div>
-          <button className="widgetSmButton">
-            <Visibility className="widgetSmIcon" />
-            Display
-          </button>
-        </li>
-        <li className="widgetSmListItem">
-          <img
-            src="https://pbs.twimg.com/media/ED9i1pXXsAAxpOh?format=jpg&name=medium"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername">Kimi Raikkonen</span>
-            <span className="widgetSmUserTitle">F1 Driver</span>
-          </div>
-          <button className="widgetSmButton">
-            <Visibility className="widgetSmIcon" />
-            Display
-          </button>
-        </li>
+        {newUsers?.map((user) => (
+          <li className="widgetSmListItem" key={user._id}>
+            <img
+              src={
+                user.profilePic ||
+                "https://i.pinimg.com/474x/bd/ee/4c/bdee4c328550aaf21aa9f43fd19e2136.jpg"
+              }
+              alt=""
+              className="widgetSmImg"
+            />
+            <div className="widgetSmUser">
+              <span className="widgetSmUsername">{user.username}</span>
+            </div>
+            <button className="widgetSmButton">
+              <Visibility className="widgetSmIcon" />
+              Display
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
