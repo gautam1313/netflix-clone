55de817caf6eade5ff8c2f7a9d934153edf4956d
Sebby80 <sebkm80@gmail.com>
2021-11-25 18:29:53 +0530

    Setting up genres

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -37,18 +37,12 @@ const Featured = ({ type, setGenre }) => {
             onChange={(e) => setGenre(e.target.value)}
           >
             <option>Genre</option>
-            <option value="adventure">Adventure</option>
+            <option value="Anime">Anime</option>
             <option value="comedy">Comedy</option>
             <option value="crime">Crime</option>
-            <option value="fantasy">Fantasy</option>
-            <option value="historical">Historical</option>
-            <option value="horror">Horror</option>
-            <option value="romance">Romance</option>
-            <option value="sci-fi">Sci-fi</option>
-            <option value="thriller">Thriller</option>
-            <option value="western">Western</option>
-            <option value="animation">Animation</option>
-            <option value="drama">Drama</option>
+            <option value="Scifi">Sci-fi</option>
+            <option value="Anime">Animation</option>
+            <option value="Sports">Drama</option>
             <option value="documentary">Documentary</option>
           </select>
         </div>
